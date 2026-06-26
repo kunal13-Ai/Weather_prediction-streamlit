662175f0ee8a204c55571d5755ee1f5ea723bd08
Kunal <kunal13shelke@gmail.com>
2026-06-26 18:00:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# 🌦 Weather Prediction App
+
+A Machine Learning web application built with Streamlit that predicts whether it will rain tomorrow or NOT based on weather conditions.
+
+## Features
+
+- Predicts rain tomorrow
+- Random Forest Classifier
+- Interactive Streamlit UI
+- Prediction probability
+
+## Tech Stack
+
+- Python
+- Pandas
+- NumPy
+- Scikit-learn
+- Streamlit
+- Joblib
+
+## Project Structure
+
+
+weather-streamlit/
+│── app.py
+│── Weather_model.pkl
+│── requirements.txt
+│── README.md
+
+
+## Installation
+
+
+git clone https://github.com/kunal13-Ai?tab=repositories
+
+cd weather-streamlit
+
+pip install -r requirements.txt
+
+streamlit run app.py
+
+
+## Model
+Algorithm: Random Forest Classifier
---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+model = joblib.load("weather_model.pkl")
+
+st.set_page_config(
+    page_title="Weather Prediction App",
+    layout="centered")
+
+st.title("🌦 Weather Rain Prediction System")
+st.write("Enter weather conditions to predict whether it will rain tomorrow.")
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    humidity = st.number_input("Humidity at 3pm", min_value=0.0, max_value=100.0, value=50.0,step=1.0)
+    rainfall = st.number_input("Rainfall (mm)", min_value=0.0, value=0.0,step=1.0)
+    sunshine = st.number_input("Sunshine (hours)", min_value=0.0, max_value=15.0,step=1.0, value=5.0)
+
+with col2:
+    cloud = st.number_input("Cloud cover at 3pm", min_value=0.0, max_value=9.0,step=1.0, value=4.0)
+    pressure = st.number_input("Pressure at 3pm", value=1015.0,step=1.0)
+    rain_today = st.selectbox("Rain Today", ["Yes", "No"])
+
+input_data = pd.DataFrame([{
+    "Humidity3pm": humidity,
+    "Rainfall": rainfall,
+    "Sunshine": sunshine,
+    "Cloud3pm": cloud,
+    "Pressure3pm": pressure,
+    "RainToday": rain_today }])
+
+st.markdown("---")
+
+if st.button("Predict Rain Tomorrow"):
+    prediction = model.predict(input_data)
+
+    if prediction[0] == 1:
+        st.error("🌧 It WILL rain tomorrow")
+    else:
+        st.success("☀ No rain expected tomorrow")
+
+    st.write("Prediction output:", prediction[0])
+
+# ----------------------------
+# Debug (optional)
+# ----------------------------
+with st.expander("Show input data"):
+    st.write(input_data)
